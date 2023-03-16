b857b069590cda29f7691db18685fed3bf601a37
Omochice <mochice.mls.ntl@gmail.com>
2023-03-16 19:15:30 +0900

    :+1: Add completion

---
+++ completions/lazyasdf.fish
@@ -0,0 +1,3 @@
+complete --command lazyasdf --no-files --condition "__fish_use_subcommand" --arguments install --description "Run `asdf install` with fzf"
+complete --command lazyasdf --no-files --condition "__fish_use_subcommand" --arguments global --description "Run `asdf global` with fzf"
+complete --command lazyasdf --no-files --condition "__fish_use_subcommand" --arguments local --description "Run `asdf local` with fzf"
